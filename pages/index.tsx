54c33144c3cfcbedda86f299f50a13b05e18e335
Brendan <smallpigeon1@gmail.com>
2022-05-30 20:05:54 -0700

    add about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import NavBar from '../components/navbar'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import NavBar from '../components/navbar';
+import styles from '../styles/Home.module.css';
+import utils from '../styles/utils.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={utils.rootContainer}>
       <NavBar selected="Projects" />
       <main className={styles.main}>
         <h2>Brendan Chen</h2>
