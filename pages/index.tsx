73a0e4d91f16f4ed67733c54a0e733e85d2f55c9
Brendan <smallpigeon1@gmail.com>
2022-05-30 20:35:00 -0700

    remove styles import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next';
 import NavBar from '../components/navbar';
-import styles from '../styles/Home.module.css';
 import utils from '../styles/utils.module.css';
 
 const Home: NextPage = () => {
   return (
     <div className={utils.rootContainer}>
       <NavBar selected="Projects" />
-      <main className={styles.main}>
+      <main>
         <h2>Brendan Chen</h2>
         <h1>Developer and Designer</h1>
       </main>
