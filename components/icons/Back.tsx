0e29d3ba400c7c11bb73318f793b9ae9e38ee33c
Brendan <smallpigeon1@gmail.com>
2022-05-31 20:44:05 -0700

    Move back button

---
+++ components/icons/Back.tsx
@@ -0,0 +1,9 @@
+import { IconProps } from './icon-types';
+
+export default function Back({ width, height }: IconProps) {
+  return (
+    <svg width={`${width}`} height={`${height}`} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15.6162 6.25L9.38406 12.4822L15.6162 18.7143" />
+    </svg>
+  );
+}
