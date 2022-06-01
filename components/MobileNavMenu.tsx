034671c2bb86377ced8e597b8bb6248f3500520c
Brendan <smallpigeon1@gmail.com>
2022-06-01 16:43:35 -0700

    add hamburger menu to close nav menu

--- components/MobileNavMenu.tsx
+++ components/MobileNavMenu.tsx
@@ -1,5 +1,6 @@
 import { NavigationSelected } from '../types';
 import styles from '../styles/MobileNavMenu.module.css';
+import MobileNavBar from './MobileNavBar';
 
 interface Props {
   selected: NavigationSelected,
@@ -11,14 +12,24 @@ interface Props {
  * Navigation menu on mobile, only appears on mobile.
  */
 export default function MobileNavMenu({ selected, visible, onClose }: Props) {
+  function handleClose() {
+    if (onClose) {
+      onClose();
+    }
+  }
+
   return (
     <div className={visible ? `${styles.container} ${styles.containerDisplay}` : `${styles.container}`}>
       <div
         className={visible ? `${styles.background} ${styles.backgroundDisplay} ${styles.fadeIn}` : `${styles.background} ${styles.fadeOut}`}
-        onClick={onClose ? () => onClose() : undefined}
+        onClick={() => handleClose()}
       />
       <div className={styles.navigationPane}>
-        
+        <MobileNavBar
+          title=""
+          button="hamburger"
+          onPress={() => handleClose()}
+        />
       </div>
     </div>
   );
