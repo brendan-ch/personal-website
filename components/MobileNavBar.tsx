b86de02dee58e21d8125690bffbc5785c4401a41
Brendan <smallpigeon1@gmail.com>
2022-05-31 20:48:56 -0700

    add support for back button

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/MobileNavBar.module.css';
 import { NavigationSelected } from '../types';
+import Back from './icons/Back';
 import Hamburger from './icons/Hamburger';
 
 interface Props {
@@ -16,9 +17,15 @@ export default function MobileNavBar({ button, selected }: Props) {
   return (
     <div className={styles.container}>
       {/* Hamburger menu */}
-      <button>
-        <Hamburger width={40} height={40} />
-      </button>
+      {button !== 'none' ? (
+        <button>
+          {button === 'hamburger' ? (
+            <Hamburger width={40} height={40} />
+          ) : (
+            <Back width={40} height={40} />
+          )}
+        </button>
+      ) : undefined}
       {/* Text */}
       <h2>{selected}</h2>
     </div>
