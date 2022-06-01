ba4d0443fc4ad892899fb59833e392d5f25e36dd
Brendan <smallpigeon1@gmail.com>
2022-05-31 20:48:40 -0700

    update back icon

--- components/icons/Back.tsx
+++ components/icons/Back.tsx
@@ -2,8 +2,8 @@ import { IconProps } from './icon-types';
 
 export default function Back({ width, height }: IconProps) {
   return (
-    <svg width={`${width}`} height={`${height}`} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.6162 6.25L9.38406 12.4822L15.6162 18.7143" />
+    <svg width={`${width}`} height={`${height}`} viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" clipRule="evenodd" d="M15.7214 19.9715L25.3393 10.3536L24.6322 9.64645L14.3072 19.9715L24.6322 30.2965L25.3393 29.5894L15.7214 19.9715Z" />
     </svg>
   );
 }
