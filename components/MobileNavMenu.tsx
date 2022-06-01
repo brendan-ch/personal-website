5f9b67a021870c86fbd269c8f9766890217918a9
Brendan <smallpigeon1@gmail.com>
2022-05-31 21:07:28 -0700

    add fade-in background

--- components/MobileNavMenu.tsx
+++ components/MobileNavMenu.tsx
@@ -11,9 +11,11 @@ interface Props {
  * Navigation menu on mobile, only appears on mobile.
  */
 export default function MobileNavMenu({ selected, visible }: Props) {
+  console.log(visible);
+
   return (
-    <div className={visible ? `${styles.container} ${styles.containerDisplay}` : styles.container}>
-      
+    <div className={visible ? `${styles.container} ${styles.containerDisplay}` : `${styles.container}`}>
+      <div className={visible ? `${styles.background} ${styles.backgroundDisplay} ${styles.fadeIn}` : `${styles.background} ${styles.fadeOut}`} />
     </div>
   );
 }
