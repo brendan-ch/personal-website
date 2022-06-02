902b1f301d8cbbd01627b476542188e84a733f31
Brendan <smallpigeon1@gmail.com>
2022-06-01 17:18:26 -0700

    add nav menu to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import MobileNavBar from '../components/MobileNavBar';
+import MobileNavMenu from '../components/MobileNavMenu';
 import NavBar from '../components/NavBar';
 import PageHeader from '../components/PageHeader';
 import utils from '../styles/utils.module.css';
 
 export default function AboutPage() {
+  // Whether the navigation menu is open
+  const [menuToggled, setMenuToggled] = useState(false);
+  const selected = "About Me";
+
   return (
     <div className={utils.rootContainer}>
-      <NavBar selected="About" />
-      <MobileNavBar selected="About" button="hamburger" />
+      <NavBar selected={selected} />
+      <MobileNavBar
+        title={selected}
+        button="hamburger"
+        onPress={() => setMenuToggled(!menuToggled)}
+      />
       <main>
         <PageHeader
           aboveText="Brendan Chen"
           belowText="About Me"
         />
       </main>
+      <MobileNavMenu
+        selected={selected}
+        visible={menuToggled}
+        onClose={() => setMenuToggled(false)}
+      />
     </div>
   )
 }
