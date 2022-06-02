4263ad510edb46575eda948e1f3a6dfaa0f220aa
Brendan <smallpigeon1@gmail.com>
2022-06-01 17:20:56 -0700

    add titles to icons

--- components/icons/Back.tsx
+++ components/icons/Back.tsx
@@ -3,6 +3,7 @@ import { IconProps } from './icon-types';
 export default function Back({ width, height }: IconProps) {
   return (
     <svg width={`${width}`} height={`${height}`} viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <title>Back Icon</title>
       <path fillRule="evenodd" clipRule="evenodd" d="M15.7214 19.9715L25.3393 10.3536L24.6322 9.64645L14.3072 19.9715L24.6322 30.2965L25.3393 29.5894L15.7214 19.9715Z" />
     </svg>
   );
