c4eeedbff663eb09af20697f543babf0e7d1518c
Brendan <smallpigeon1@gmail.com>
2022-06-14 15:44:38 -0700

    add DatabaseItem type

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,16 @@
+interface DatabaseItem {
+  title: string,
+  description: string,
+  imageLink: string | null,
+  imagePosX: number | null,
+  imagePosY: number | null,
+  /**
+   * Notion database ID of the item.
+   */
+  id: string,
+  tags: string[],
+}
+
 type NavigationSelected = 'Projects' | 'About Me';
 
-export { NavigationSelected };
+export { NavigationSelected, DatabaseItem };
