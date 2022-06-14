84c4fe77c67b2d3b671c3b4d2a1ce110291ef794
Brendan <smallpigeon1@gmail.com>
2022-06-14 16:18:17 -0700

    update Database component with ListItem

--- components/Database.tsx
+++ components/Database.tsx
@@ -1,6 +1,8 @@
 import { DatabaseItem } from '../types';
 import styles from '../styles/Database.module.css';
 import Down from './icons/Down';
+import { useState } from 'react';
+import ListItem from './ListItem';
 
 interface Props {
   items: DatabaseItem[],
@@ -11,18 +13,31 @@ interface Props {
 }
 
 export default function Database({ items, tagOptions }: Props) {
+  const [tag, setTag] = useState(tagOptions.length > 0 ? tagOptions[0] : undefined);
+
+  const filteredItems = tag ? items.filter((value) => value.tags.includes(tag)) : [];
+
   return (
     <div className={styles.container}>
       <div className={styles.dropdownButtonContainer}>
         <button className={styles.dropdownButton}>
-          <p>Featured</p>
+          <p>{tag}</p>
           <Down
             width={40}
             height={40}
           />
         </button>
       </div>
-      <div className={styles.line} />
+      <div className={styles.divider} />
+      {/* Content */}
+      {filteredItems.map((item) => (
+        <ListItem
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          imageLink={item.imageLink}
+        />
+      ))}
     </div>
   );
 }
