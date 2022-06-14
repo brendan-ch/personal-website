ae93192a48b2ef990bb7ac0acc00dde7bb3a756b
Brendan <smallpigeon1@gmail.com>
2022-06-14 16:42:22 -0700

    update font varians

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:wght@400;500&display=swap" rel="stylesheet" />
           <meta name="robots" content="noindex,nofollow"></meta>
         </Head>
         <body>
