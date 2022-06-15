97a1ba544925678f307e6d1e25136b802b3f6848
Brendan <smallpigeon1@gmail.com>
2022-06-14 17:14:58 -0700

    update dropdown listener

---
+++ components/Dropdown.tsx
@@ -0,0 +1,37 @@
+import styles from '../styles/Dropdown.module.css';
+
+interface Props {
+  options: string[],
+  onSelect: (selected: string) => any,
+  onClose?: () => any,
+  top: number,
+  left: number,
+  visible?: boolean,
+}
+
+/**
+ * Dropdown content. Should be wrapped 
+ * @returns
+ */
+export default function Dropdown({ top, left, options, onClose, onSelect, visible }: Props) {
+  return (
+    <div
+      className={styles.clickListener}
+      onClick={onClose ? () => onClose() : undefined}
+    >
+      <div
+        className={visible ? `${styles.container} ${styles.containerVisible}` : styles.container}
+        style={{
+          top,
+          left,
+        }}
+      >
+        {options.map((value, index) => (
+          <button className={styles.dropdownItem} key={index}>
+            <p>{value}</p>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
