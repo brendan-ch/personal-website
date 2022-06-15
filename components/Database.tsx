09b20cffc0d56a4990aa7fe7eae51e5b9d9656a0
Brendan <smallpigeon1@gmail.com>
2022-06-14 18:32:41 -0700

    add basic dropdown menu

--- components/Database.tsx
+++ components/Database.tsx
@@ -1,8 +1,9 @@
 import { DatabaseItem } from '../types';
 import styles from '../styles/Database.module.css';
 import Down from './icons/Down';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import ListItem from './ListItem';
+import Dropdown from './Dropdown';
 
 interface Props {
   items: DatabaseItem[],
@@ -12,15 +13,50 @@ interface Props {
   tagOptions: string[],
 }
 
+/**
+ * @todo Move dropdown implementation and state up to projects page
+ */
 export default function Database({ items, tagOptions }: Props) {
   const [tag, setTag] = useState(tagOptions.length > 0 ? tagOptions[0] : undefined);
 
+  const [top, setTop] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [visible, setVisible] = useState(false);
+
   const filteredItems = tag ? items.filter((value) => value.tags.includes(tag)) : [];
+
+  /**
+   * Handle the press of the dropdown menu button.
+   * @param e
+   */
+  function handleButtonPress(e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) {
+    // @ts-ignore
+    const button: HTMLButtonElement = e.target;
+
+    console.log(button.offsetTop);
+    console.log(button.offsetLeft);
+
+    setTop(button.offsetTop);
+    setLeft(button.offsetLeft);
+    setVisible(true);
+  }
+
 
   return (
     <div className={styles.container}>
+      <Dropdown
+        top={top}
+        left={left}
+        visible={visible}
+        options={['Featured', 'All Projects']}
+        onSelect={() => {}}
+        onClose={() => setVisible(false)}
+      />
       <div className={styles.dropdownButtonContainer}>
-        <button className={styles.dropdownButton}>
+        <button
+          className={styles.dropdownButton}
+          onClick={(e) => handleButtonPress(e)}
+        >
           <p>{tag}</p>
           <Down
             width={40}
