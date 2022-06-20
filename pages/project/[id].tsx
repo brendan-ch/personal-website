1395f3d1a795bb94b81b7af31828cdb9b03f11d3
Brendan <smallpigeon1@gmail.com>
2022-06-20 11:44:24 -0700

    add getStaticPaths and getStaticProps for project page

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,5 +1,82 @@
+import { PROJECTS_DATABASE_ID, REVALIDATE } from '../../helpers/Constants';
+import getChildrenBlocks from '../../helpers/getChildrenBlocks';
+import getDatabaseBlocks from '../../helpers/getDatabaseBlocks';
 
+export const getStaticPaths = async () => {
+  // Get pages in database
+  const items = await getDatabaseBlocks(PROJECTS_DATABASE_ID, {
+    and: [
+      {
+        property: 'Published',
+        checkbox: {
+          equals: true,
+        },
+      },
+    ],
+  });
 
-export default function ProjectPage() {
-  
+  return {
+    paths: items.map((value) => ({
+      params: {
+        id: value.id,
+        title: value.title,
+      }
+    })),
+    fallback: true,
+  }
+};
+
+export const getStaticProps = async ({ params }: { params: any }) => {
+  let blocks = null;
+
+  if (!params || typeof params.id !== 'string') return {
+    props: {
+      blocks,
+      title: null,
+      lastRegenerated: Date.now(),
+    }
+  };
+
+  // Get block data
+  try {
+    blocks = await getChildrenBlocks(params.id);
+  } catch(e) {
+    return {
+      props: {
+        blocks,
+        title: null,
+        lastRegenerated: Date.now(),
+      }
+    };
+  }
+
+  return {
+    props: {
+      blocks,
+      title: null,
+      lastRegenerated: Date.now(),
+    },
+    revalidate: REVALIDATE,
+  };
+};
+
+interface Props {
+  blocks?: any[],
+  title?: string,
+  /**
+   * Time in milliseconds when the page was last regenerated.
+   */
+  lastRegenerated: number,
+}
+
+/**
+ * Page that displays project information.
+ * @returns
+ */
+export default function ProjectPage({ blocks, title, lastRegenerated }: Props) {
+  return (
+    <div>
+      <p>Hello there</p>
+    </div>
+  );
 }
