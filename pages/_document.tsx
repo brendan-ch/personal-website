c623a9e12d99b59a34ef7a1170b71e12a7116d8c
Brendan <smallpigeon1@gmail.com>
2022-06-21 17:55:56 -0700

    Allow web indexing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link rel="stylesheet" href="https://use.typekit.net/upd1iml.css" />
-          <meta name="robots" content="noindex,nofollow"></meta>
           <meta name="description" content="Hi there! I’m Brendan, a designer and developer living in Orange, California."></meta>
           <meta name="og:image" content="/link-preview-image.png"></meta>
         </Head>
