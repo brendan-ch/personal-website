807005240d6fd8270b51c6af294e299e2e91f200
Brendan <smallpigeon1@gmail.com>
2022-06-22 16:23:21 -0700

    link to pretty link

--- components/Database.tsx
+++ components/Database.tsx
@@ -32,7 +32,7 @@ export default function Database({
           title={item.title}
           description={item.description}
           imageLink={item.imageLink}
-          link={`/project/${item.id}`}
+          link={`/project/${item.prettyLink}`}
         />
       ))}
     </div>
