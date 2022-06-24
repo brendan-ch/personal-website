5d7f060c2747cacfd64ff2f78076ab7cbbdf75c4
Brendan <smallpigeon1@gmail.com>
2022-06-23 18:03:07 -0700

    add updateImageBlocks helper

---
+++ helpers/updateImageBlocks.ts
@@ -0,0 +1,26 @@
+import client from './notionClient';
+import { UpdatedBlockItem } from '../types';
+
+/**
+ * Write preview images back to Notion.
+ */
+export default async function updateImageBlocks(updatedItems: UpdatedBlockItem[]) {
+  for (let i = 0; i < updatedItems.length; i++) {
+    const item = updatedItems[i];
+    console.log(item);
+
+    try {
+      await client.blocks.update({
+        block_id: item.blockId,
+        image: {
+          caption: item.caption,
+          external: {
+            url: item.imageLink,
+          },
+        },
+      });
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
