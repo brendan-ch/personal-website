030a04483c181b24dbba17e7a424f00f36a488b4
Brendan <smallpigeon1@gmail.com>
2022-06-23 20:00:30 -0700

    update environment variables

--- helpers/aws/S3Client.ts
+++ helpers/aws/S3Client.ts
@@ -1,5 +1,11 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { REGION } from '../Constants';
 
-const s3Client = new S3Client({ region: REGION });
+const s3Client = new S3Client({
+  region: REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_SDK_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SDK_SECRET_ACCESS_KEY!,
+  },
+});
 export default s3Client;
