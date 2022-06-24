8399c652178aa8e863fd42c59d70e4c1f9ba7691
Brendan <smallpigeon1@gmail.com>
2022-06-23 19:46:43 -0700

    add code for updating preview images

--- helpers/updatePreviewImages.ts
+++ helpers/updatePreviewImages.ts
@@ -3,7 +3,33 @@
 // and returns array of database items with updated images
 
 import { DatabaseItem } from '../types';
+import uploadPreviewImages from './aws/uploadPreviewImages';
+import { BUCKET_NAME } from './Constants';
+import writePreviewImages from './writePreviewImages';
 
-export default async function updateImageBlocks(items: DatabaseItem[]) {
+export default async function updatePreviewImages(items: DatabaseItem[]) {
+  const copy: DatabaseItem[] = JSON.parse(JSON.stringify(items));
 
+  // Upload to AWS
+  const updated = await uploadPreviewImages(items);
+  // Write back to Notion
+  await writePreviewImages(updated);
+
+  // Write to memory
+  copy.forEach((item) => {
+    if (item.imageLink && !item.imageLink?.startsWith(`https://${BUCKET_NAME}`)) {
+      // Look for new image URL
+      // Pretty much guaranteed to be 0
+      const updatedItemIndex = updated.findIndex((value) => value.pageId === item.id);
+      
+      if (updated[updatedItemIndex]?.imageLink) {
+        // Update item
+        item.imageLink = updated[updatedItemIndex].imageLink;
+      }
+
+      updated.splice(updatedItemIndex, 1);
+    }
+  });
+
+  return copy;
 }
