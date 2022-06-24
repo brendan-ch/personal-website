5dbffcdf97f8b992e9aa0768bda23ea891f4b65b
Brendan <smallpigeon1@gmail.com>
2022-06-23 17:53:47 -0700

    add uploadImageBlocks function

---
+++ helpers/aws/uploadImageBlocks.ts
@@ -0,0 +1,52 @@
+import { PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
+import { UpdatedBlockItem } from "../../types";
+import { BUCKET_NAME, REGION } from "../Constants";
+import s3Client from "./S3Client";
+
+/**
+ * Takes an array of blocks, and uploads images to AWS.
+ * Returns an array of block objects to update.
+ * @param items
+ * @returns
+ */
+export default async function uploadImageBlocks(blocks: any[]): Promise<UpdatedBlockItem[]> {
+  let updated: UpdatedBlockItem[] = [];
+
+  const blocksWithImage = blocks.filter((block) => block.type === 'image' && block.image.type === 'file');
+
+  // Use regular for loop
+  for (let i = 0; i < blocksWithImage.length; i++) {
+    try {
+      const blockContent = blocksWithImage[i].image;
+      const id = blocksWithImage[i].id;
+
+      // Download the image
+      const response = await fetch(blockContent.file.url, {
+        method: 'GET',
+      });
+
+      const arrayBuffer = await response.arrayBuffer();
+      const params: PutObjectCommandInput = {
+        Bucket: BUCKET_NAME,
+        Key: `blocks/${id}`,
+        Body: Buffer.from(arrayBuffer),
+        ACL: 'public-read',
+      };
+
+      const result = await s3Client.send(new PutObjectCommand(params));
+
+      // Add to updated
+      updated.push({
+        imageLink: `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/blocks/${id}`,
+        imageName: id,
+        blockId: id,
+      });
+      
+    } catch(e) {
+      // Log and manually resolve later
+      console.error(e);
+    }
+  }
+
+  return updated;
+}
