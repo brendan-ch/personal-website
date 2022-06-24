d1132f0c004a2ff6b9d73014618eea437108e4fb
Brendan <smallpigeon1@gmail.com>
2022-06-23 18:13:51 -0700

    add placeholder functions

---
+++ helpers/updateImageBlocks.ts
@@ -0,0 +1,7 @@
+// To-do: write convenience function that takes array of blocks,
+// uploads images to AWS, writes image blocks back to Notion,
+// and returns array with updated images
+
+export default async function updateImageBlocks(blocks: any[]) {
+
+}
---
+++ helpers/updatePreviewImages.ts
@@ -0,0 +1,9 @@
+// To-do: write convenience function that takes array of database items,
+// uploads preview images to AWS, writes preview images back to Notion,
+// and returns array of database items with updated images
+
+import { DatabaseItem } from '../types';
+
+export default async function updateImageBlocks(items: DatabaseItem[]) {
+
+}
