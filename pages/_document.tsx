57e7beb34ccc6d9a740c25f5b276fef6c6306a8b
Brendan <smallpigeon1@gmail.com>
2022-06-27 16:13:15 -0700

    add twitter descriptions and images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,13 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link rel="stylesheet" href="https://use.typekit.net/upd1iml.css" />
           <meta name="description" content="I'm Brendan, a developer and designer living in Orange, California."></meta>
+
           <meta name="og:description" content="I'm Brendan, a developer and designer living in Orange, California."></meta>
           <meta name="og:image" content="/link-preview-image.png"></meta>
+
+          <meta name="twitter:description" content="I'm Brendan, a developer and designer living in Orange, California."></meta>
+          <meta name="twitter:image" content="/link-preview-image.png"></meta>
+
           <meta name="google-site-verification" content="4f6AZxwJwKFUlOFOJdcfAbCKMqxOiEcGQ4N7NefSsI4"></meta>
           {/* Fallback title */}
           {/* eslint-disable-next-line */}
