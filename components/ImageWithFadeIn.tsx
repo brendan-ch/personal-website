7d5f268c305578368778f750d55629fabef83212
Brendan <smallpigeon1@gmail.com>
2022-06-29 23:09:20 -0700

    NO MORE FLICKERING LETSGO

--- components/ImageWithFadeIn.tsx
+++ components/ImageWithFadeIn.tsx
@@ -1,7 +1,13 @@
 import Image, { ImageProps } from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '../styles/ImageWithFadeIn.module.css';
+
+enum LoadingState {
+  BEFORE,
+  LOADING,
+  DONE,
+}
 
 /**
  * Component that fades in an image as soon as it loads.
@@ -11,13 +17,25 @@ import styles from '../styles/ImageWithFadeIn.module.css';
  */
 export default function ImageWithFadeIn(props: ImageProps) {
   const [loaded, setLoaded] = useState(false);
+  const [loadingState, setLoadingState] = useState(LoadingState.BEFORE);
+
+  useEffect(() => {
+    setLoadingState(LoadingState.LOADING);
+  }, []);
+
+  let className = `${props.className}`;
+  if (loadingState === LoadingState.LOADING) {
+    className = `${props.className} ${styles.invisible}`;
+  } else if (loadingState === LoadingState.DONE) {
+    className = `${props.className} ${styles.fadeIn}`;
+  }
   
   return (
     /* eslint-disable-next-line */
     <Image
       {...props}
-      className={loaded ? `${props.className} ${styles.fadeIn}` : `${props.className}`}
-      onLoad={() => setLoaded(true)}
+      className={className}
+      onLoadingComplete={() => setLoadingState(LoadingState.DONE)}
     />
   );
 }
