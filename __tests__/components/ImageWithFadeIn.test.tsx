21d5d827e7a828dc12f125f2531f1b1cc7307f70
Brendan <smallpigeon1@gmail.com>
2022-07-01 16:54:58 -0700

    add testing for fading image

---
+++ __tests__/components/ImageWithFadeIn.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import ImageWithFadeIn from "../../components/ImageWithFadeIn";
+import '@testing-library/jest-dom';
+
+// Component tests for the ImageWithFadeIn component
+describe('ImageWithFadeIn', () => {
+  it('renders an image without erroring', () => {
+    // Render the element
+    const { getByRole } = render(<ImageWithFadeIn
+      src="/link-preview-image.png"
+      alt="Testing Image"
+      layout="fill"
+    />);
+
+    // Expect there to be an image
+    expect(getByRole('img')).toBeInTheDocument();
+  });
+
+  it('sets initial class to invisible', async () => {
+    // Instead of checking for state change
+    // Check for whether the image styling changes
+    const { debug, getByRole } = render(<ImageWithFadeIn
+      src="/link-preview-image.png"
+      alt="Testing Image"
+      layout="fill"
+    />);
+
+    debug();
+    // Wait for the CSS to change
+    await waitFor(() => expect(getByRole('img')).toHaveClass('invisible'));
+  });
+});
