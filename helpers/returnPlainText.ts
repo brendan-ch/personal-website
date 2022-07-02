4f119eaaf988025366c094741fb86882b2d51b68
Brendan <smallpigeon1@gmail.com>
2022-07-01 20:51:51 -0700

    add edge case for no richTextItems passed

--- helpers/returnPlainText.ts
+++ helpers/returnPlainText.ts
@@ -5,6 +5,7 @@
  */
 function returnPlainText(richTextItems: any[]) {
   let plainText = '';
+  if (!richTextItems || richTextItems.length === 0) return plainText;
 
   richTextItems.forEach((item) => {
     plainText += item.plain_text;
