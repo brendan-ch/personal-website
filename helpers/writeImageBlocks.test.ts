840804fbabff5491cea07328ee2f927a078934c2
Brendan <smallpigeon1@gmail.com>
2022-07-04 12:40:27 -0700

    add mockReset call

--- helpers/writeImageBlocks.test.ts
+++ helpers/writeImageBlocks.test.ts
@@ -1,7 +1,11 @@
 import { updateBlocks } from '../__mocks__/@notionhq/client';
 import writeImageBlocks from './writeImageBlocks';
 
+beforeEach(() => {
+  updateBlocks.mockReset();
+});
 describe('writeImageBlocks', () => {
+
   it('Calls the `client.blocks.update` method', () => {
     writeImageBlocks([
       {
