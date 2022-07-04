111e425f7bd34ca5704ada1a0013f137ada4b804
Brendan <smallpigeon1@gmail.com>
2022-07-04 11:55:28 -0700

    test that the function works

--- helpers/getChildrenBlocks.test.ts
+++ helpers/getChildrenBlocks.test.ts
@@ -1,8 +1,14 @@
+import { MAX_RECURSION_DEPTH } from './Constants';
 import getChildrenBlocks from './getChildrenBlocks';
 
 describe('getChildrenBlocks', () => {
   it('Recursively gets children blocks', async () => {
     const result = await getChildrenBlocks('0');
-    console.log(result);
+    expect(result).toBeDefined();
+  });
+
+  it('Returns nothing if recursion depth exceeded', async () => {
+    const result = await getChildrenBlocks('0', MAX_RECURSION_DEPTH + 1);
+    expect(result).toBeUndefined();
   });
 });
