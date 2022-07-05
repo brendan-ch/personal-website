7a000e5565c4ed23b63abf1cb14e3a4d66fba7a5
Brendan <smallpigeon1@gmail.com>
2022-07-05 15:31:06 -0700

    update index page test

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import Projects from './index.page';
 import { ProjectDatabaseItem } from '../types';
+import '@testing-library/jest-dom';
 
 describe('Projects', () => {
   const dbItems: ProjectDatabaseItem[] = [
@@ -11,8 +12,8 @@ describe('Projects', () => {
     }
   ];
   
-  it('renders the page', () => {
+  it('Renders the page', () => {
     const { container } = render(<Projects dbItems={dbItems} />);
-    expect(container).toBeDefined();
+    expect(container).toBeInTheDocument();
   });
 });
