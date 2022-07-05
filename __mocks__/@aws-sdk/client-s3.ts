d8e5c8213b9b52bb1727d25dab7cc7350124ea09
Brendan <smallpigeon1@gmail.com>
2022-07-05 15:30:47 -0700

    add mocks for s3 client

--- __mocks__/@aws-sdk/client-s3.ts
+++ __mocks__/@aws-sdk/client-s3.ts
@@ -1,5 +1,16 @@
-export class S3Client {
-  constructor() {
-
+export class PutObjectCommand {
+  constructor(obj: any) {
+    
   }
 }
+
+export class S3Client {
+  readonly send = () => {};
+  
+  constructor(obj: any) {
+    // Blank constructor
+  }
+}
+
+const client = { S3Client };
+export default client;
