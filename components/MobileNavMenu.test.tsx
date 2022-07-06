51b8a4d8c81fd9bf1c6f109bdb730e2adb7023d5
Brendan <smallpigeon1@gmail.com>
2022-07-06 16:18:59 -0700

    add tests for nav menu and button

---
+++ components/MobileNavMenu.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import MobileNavMenu, { MobileNavMenuButton } from './MobileNavMenu';
+import '@testing-library/jest-dom';
+
+describe('MobileNavMenuButton', () => {
+  it('Renders a link to specifed href', () => {
+    render(<MobileNavMenuButton
+      text="Testing"
+      href="/test-link"
+    />);
+
+    const a = screen.getByRole('link');
+    expect(a).toHaveAttribute('href', '/test-link');
+  });
+
+  it('Renders the correct text', () => {
+    render(<MobileNavMenuButton
+      text="Testing"
+      href="/test-link"
+    />);
+
+    const text = screen.getByText("Testing");
+    expect(text).toBeInTheDocument();
+  });
+});
+
+describe('MobileNavMenu', () => {
+  it('Renders without passing selected', () => {
+    render(<MobileNavMenu visible />);
+    
+    expect(screen.getByRole('menu')).toBeInTheDocument();
+  });
+
+  it('Renders with passing selected', () => {
+    render(<MobileNavMenu visible selected="About Me" />);
+    
+    expect(screen.getByRole('menu')).toBeInTheDocument();
+  });
+});
