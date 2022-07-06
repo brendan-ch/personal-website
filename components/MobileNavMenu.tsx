db86d21f8e8772a4bc8a4b2d120168cf6bef49cc
Brendan <smallpigeon1@gmail.com>
2022-07-06 16:05:41 -0700

    add button links and background image

--- components/MobileNavMenu.tsx
+++ components/MobileNavMenu.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/MobileNavMenu.module.css';
 import Footer from './Footer';
 
@@ -7,20 +9,22 @@ interface ButtonProps {
   href: string,
 }
 
-export function MobileNavMenuButton({ toggled, text }: ButtonProps) {
+export function MobileNavMenuButton({ toggled, text, href }: ButtonProps) {
   return (
-    <div className={styles.buttonWrapper}>
-      <div className={styles.line} />
-      <div className={styles.buttonTextContainer}>
-        <h3>
-          {toggled ? (
-            <b>
-              {text}
-            </b>
-          ) : text}
-        </h3>
-      </div>
-    </div>
+    <Link href={href}>
+      <a className={styles.buttonWrapper}>
+        <div className={styles.line} />
+        <div className={styles.buttonTextContainer}>
+          <h3 className={toggled ? styles.red : undefined}>
+            {toggled ? (
+              <b>
+                {text}
+              </b>
+            ) : text}
+          </h3>
+        </div>
+      </a>
+    </Link>
   );
 }
 
@@ -37,21 +41,36 @@ interface Props {
 export default function MobileNavMenu({ selected, visible }: Props) {
   return (
     <div className={visible ? `${styles.container} ${styles.containerVisible}` : styles.container}>
-      <div className={styles.buttons}>
-        <MobileNavMenuButton
-          toggled={selected === 'Featured'}
-          text="Home"
-          href="/"
-        />
-        {/* <MobileNavMenuButton /> */}
-        {/* <MobileNavMenuButton /> */}
-        <MobileNavMenuButton
-          toggled={selected === 'About Me'}
-          text="About Me"
-          href="/about"
+      <Image
+        src={require('../public/background-pattern.png')}
+        layout="fill"
+        objectFit="cover"
+        style={{
+          zIndex: 2
+        }}
+        alt="Background image"
+      />
+      <div className={styles.contentContainer}>
+        <div className={styles.buttons}>
+          <MobileNavMenuButton
+            toggled={selected === 'Featured'}
+            text="Home"
+            href="/"
+          />
+          {/* <MobileNavMenuButton /> */}
+          {/* <MobileNavMenuButton /> */}
+          <MobileNavMenuButton
+            toggled={selected === 'About Me'}
+            text="About Me"
+            href="/about"
+          />
+        </div>
+        <Footer
+          style={{
+            backgroundColor: 'transparent',
+          }}
         />
       </div>
-      <Footer />
     </div>
   );
 }
