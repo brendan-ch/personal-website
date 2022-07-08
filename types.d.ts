feb4e43f614e0bf8d8cb48c254ff24b0e44bcd03
Brendan <smallpigeon1@gmail.com>
2022-07-08 15:32:04 -0700

    remove NotionCodeLanguage enum

--- types.d.ts
+++ types.d.ts
@@ -66,12 +66,8 @@ interface NotionFileData extends NotionBlockDataWithChildren {
   caption?: RichTextObject[],
 }
 
-enum NotionCodeLanguage {
-  "abap", "arduino", "bash", "basic", "c", "clojure", "coffeescript", "c++", "c#", "css", "dart", "diff", "docker", "elixir", "elm", "erlang", "flow", "fortran", "f#", "gherkin", "glsl", "go", "graphql", "groovy", "haskell", "html", "java", "javascript", "json", "julia", "kotlin", "latex", "less", "lisp", "livescript", "lua", "makefile", "markdown", "markup", "matlab", "mermaid", "nix", "objective-c", "ocaml", "pascal", "perl", "php", "plain text", "powershell", "prolog", "protobuf", "python", "r", "reason", "ruby", "rust", "sass", "scala", "scheme", "scss", "shell", "sql", "swift", "typescript", "vb.net", "verilog", "vhdl", "visual basic", "webassembly", "xml", "yaml", "java/c/c++/c#"
-}
-
 interface NotionCodeData extends NotionTextData {
-  language: NotionCodeLanguage,
+  language: string,
 }
 
 type NotionBlockData = NotionBlockDataWithChildren | NotionTextData | NotionBulletedData | NotionCalloutData | NotionFileData;
