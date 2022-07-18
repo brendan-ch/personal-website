2f9f0549d1f8d4c4a2ee37e118e9629255767685
Brendan <smallpigeon1@gmail.com>
2022-07-17 20:48:36 -0700

    change let to const

--- pages/doc/[prettyLink].page.tsx
+++ pages/doc/[prettyLink].page.tsx
@@ -11,7 +11,7 @@ import getPage from '../../helpers/getPage';
 
 export const getStaticPaths = async () => {
   // Get pages
-  let items = await getPages({
+  const items = await getPages({
     prefix: 'doc',
     filter: [
       {
