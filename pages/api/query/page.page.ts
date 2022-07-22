02a2f34d823453b37e6a98681f588fbe013bc0b3
Brendan <smallpigeon1@gmail.com>
2022-07-21 20:33:09 -0700

    add error case

--- pages/api/query/page.page.ts
+++ pages/api/query/page.page.ts
@@ -6,34 +6,43 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>,
 ) {
-  // Share interface with code
-  const query: PageQuery = req.body;
-  delete query.withContent;
+  try {
+    // Share interface with code
+    const query: PageQuery = req.body;
+    delete query.withContent;
 
-  // Validate
-  if (
-    !query
-    || (typeof query.id !== 'string'
-    || !['string', 'undefined'].includes(typeof query.prefix)
-    || !['boolean', 'undefined'].includes(typeof query.withContent))
-  ) {
-    // 400 error
-    const error400: Response = {
-      successful: false,
-      error: 'Invalid request. Double check the request body and try again.',
+    // Validate
+    if (
+      !query
+      || (typeof query.id !== 'string'
+      || !['string', 'undefined'].includes(typeof query.prefix)
+      || !['boolean', 'undefined'].includes(typeof query.withContent))
+    ) {
+      // 400 error
+      const error400: Response = {
+        successful: false,
+        error: 'Invalid request. Double check the request body and try again.',
+      };
+      // Return 400 error
+      return res.status(400).json(error400);
+    }
+
+    const pageData = await getPage(query);
+    
+    const response: Response = {
+      successful: true,
+      data: {
+        ...pageData,
+      },
     };
-    // Return 400 error
-    return res.status(400).json(error400);
+
+    return res.status(200).json(response);
+  } catch(e) {
+    const error500: Response = {
+      successful: false,
+      error: 'Internal server error.',
+    };
+
+    return res.status(500).json(error500);
   }
-
-  const pageData = await getPage(query);
-  
-  const response: Response = {
-    successful: true,
-    data: {
-      ...pageData,
-    },
-  };
-
-  res.status(200).json(response);
 }
