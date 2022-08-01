4b74233903f453b74b4044550f39a304b2373faa
Brendan <smallpigeon1@gmail.com>
2022-08-01 10:21:50 -0700

    add placeholder component

---
+++ components/GalleryItemPlaceholder.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/GalleryItemPlaceholder.module.css';
+
+export default function GalleryItemPlaceholder() {
+  const children = (
+    <div className={styles.container}>
+      <div className={styles.imagePlaceholder}></div>
+      <div className={styles.details}>
+        <div className={styles.titlePlaceholder}></div>
+        <div className={styles.descriptionPlaceholder}></div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className={styles.container}>
+      {children}
+    </div>
+  );
+}
