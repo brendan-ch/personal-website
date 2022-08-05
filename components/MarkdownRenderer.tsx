241b99e02df7ca425e5cc486dcf37bf5f5c6d3d7
Brendan <smallpigeon1@gmail.com>
2022-08-04 17:26:33 -0700

    add comma

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -61,7 +61,7 @@ export default function MarkdownRenderer({ content, onImageClick, imageAspectRat
                 />
               </div>
             );
-          }
+          },
         }}
         skipHtml
       >
