ef114aba0acfd832b4a174e5d60597b9af4ed8ad
Brendan <smallpigeon1@gmail.com>
2022-08-13 13:15:29 -0700

    remove unused code

--- helpers/getPage.ts
+++ helpers/getPage.ts
@@ -6,8 +6,6 @@ import { PageData, PageQuery } from '../types';
  */
 export default async function getPage(query: PageQuery): Promise<PageData> {
   // Filter generated data
-  // const data = await readFile(path.join(process.cwd(), 'scripts', 'output', 'data.json'));
-  // const parsed: any = JSON.parse(data.toString());
   // @ts-ignore
   const parsed = require('../scripts/output/data.json');
 
@@ -22,23 +20,4 @@ export default async function getPage(query: PageQuery): Promise<PageData> {
   }
 
   return correctPage!;
-
-
-  // const fileDirectory = query.prefix ? path.join(CONTENT_DIRECTORY, query.prefix, `${query.id}.md`) : path.join(CONTENT_DIRECTORY, `${query.id}.md`);
-  // const fileContents = await readFile(fileDirectory);
-
-  // // Parse metadata
-  // const matterResult: any = matter(fileContents);
-
-  // let content = null;
-  // if (query.withContent) {
-  //   content = matterResult.content;
-  // }
-
-  // return {
-  //   id: query.id,
-  //   prefix: query.prefix || null,
-  //   content,
-  //   ...matterResult.data,
-  // }
 }
