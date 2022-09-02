167bbf1cbd48655f77628aa2faf9dca43e45201e
Brendan <smallpigeon1@gmail.com>
2022-09-01 20:50:50 -0700

    add text input component

---
+++ components/TextInput.tsx
@@ -0,0 +1,31 @@
+import styles from '../styles/TextInput.module.css';
+import { useState } from "react";
+
+interface Props {
+  error?: string,
+  onBlur?: (text: string) => any,
+  placeholder?: string,
+  showError?: boolean,
+}
+
+export default function TextInput({ error, onBlur, placeholder, showError }: Props) {
+  const [text, setText] = useState('');
+
+  return (
+    <div>
+      {/* Input */}
+      <input
+        className={styles.input}
+        type="text"
+        placeholder={placeholder}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      >
+      </input>
+      {/* Error message */}
+      {showError ? (
+        <p className={styles.error}>{error}</p>
+      ) : undefined}
+    </div>
+  );
+}
