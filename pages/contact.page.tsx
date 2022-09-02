b4eebffced65013d111ff029afafb928cc504319
Brendan <smallpigeon1@gmail.com>
2022-09-01 22:51:05 -0700

    add status renderer

--- pages/contact.page.tsx
+++ pages/contact.page.tsx
@@ -7,20 +7,82 @@ import MobileNavBar from '../components/MobileNavBar';
 import MobileNavMenu from '../components/MobileNavMenu';
 import { useState } from 'react';
 import PageHeader from '../components/PageHeader';
+import axios from 'axios';
+import Link from 'next/link';
+
+enum FormState {
+  INCOMPLETE,
+  SUBMITTED,
+  SERVER_ERROR,
+  USER_ERROR,
+}
 
 export default function ContactForm() {
   const [menuVisible, setMenuVisible] = useState(false);
 
+  const [loading, setLoading] = useState(false);
+  const [formState, setFormState] = useState(FormState.INCOMPLETE);
+  const [successful, setSuccessful] = useState(false);
+
   // To-do: lift data object up
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     // Retrieve form data
     event.preventDefault();
+    setLoading(true);
 
     const formData = new FormData(event.currentTarget);
     const fieldValues = Object.fromEntries(formData.entries());
-    console.log(fieldValues)
+
+    try {
+      // API request
+      const result = await axios.post('/api/submit/contact', fieldValues);
+
+      if (result.status === 400) {
+        setFormState(FormState.USER_ERROR);
+      } else if (result.status === 500) {
+        setFormState(FormState.SERVER_ERROR);
+      } else {
+        setFormState(FormState.SUBMITTED);
+      }
+
+      setLoading(false);
+    } catch(e) {
+      setFormState(FormState.USER_ERROR);
+    }
   }
   
+  let statusRenderer: JSX.Element = <></>;
+
+  switch (formState) {
+    case FormState.SERVER_ERROR:
+      statusRenderer = (
+        <p>
+          There was an internal server error. Please try again later.
+        </p>
+      );
+      break;
+    case FormState.USER_ERROR:
+      statusRenderer = (
+        <p>
+          There was an issue sending your message. Please double-check your fields and try again.
+        </p>
+      );
+      break;
+    case FormState.SUBMITTED:
+      statusRenderer = (
+        <p>
+          Your message has been submitted.{' '}
+          <Link href="/">
+            <a>
+              <u>
+                Return to Home
+              </u>
+            </a>
+          </Link>
+        </p>
+      );
+  }
+
   return (
     <div className={utils.rootContainer}>
       <Head>
@@ -64,25 +126,24 @@ export default function ContactForm() {
               name="subject"
               label="Subject"
               placeholder="Subject"
-              pattern={/.+/}
-              noMatchError="This field is required."
               required
             />
-            {/* To-do: make this multiline */}
             <FormInput
               name="message"
               multiline
               label="Message"
-              pattern={/.+/}
-              noMatchError="This field is required."
+              pattern={/.{16,}/}
+              noMatchError="Message must be at least 16 characters."
               placeholder="Your message"
               required
             />
-            {/* To-do: add onSubmit prop here */}
             <PageButton
               highlighted
-              text="Submit"
+              disabled={loading || formState === FormState.SUBMITTED}
+              text={loading ? 'Sending message...' : 'Send Message'}
             />
+            {/* Status */}
+            {statusRenderer}
           </form>
         </div>
       </main>
