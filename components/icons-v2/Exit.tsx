37eb86bfa0bf84117591079fed4d1b051aeebb7f
Brendan <smallpigeon1@gmail.com>
2022-09-30 12:16:39 -0700

    add new icons

---
+++ components/icons-v2/Exit.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function Exit(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <title>Exit</title>
+      <path fillRule="evenodd" clipRule="evenodd" d="M14.5858 16L1.29289 2.70711L2.70711 1.29289L16 14.5858L29.2929 1.29289L30.7071 2.70711L17.4142 16L30.7071 29.2929L29.2929 30.7071L16 17.4142L2.70711 30.7071L1.29289 29.2929L14.5858 16Z" fill="#121212"/>
+    </svg>
+  );
+}
+
+export default Exit;
