b7548705428d1cbc72466d48d98e39fee0674291
Brendan <smallpigeon1@gmail.com>
2022-09-29 22:01:01 -0700

    add github logo support

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -4,44 +4,55 @@ import AppStore from './logos/AppStore';
 import Figma from './logos/Figma';
 import GooglePlay from './logos/GooglePlay';
 import { PageExternalLink } from '../types';
+import GitHub from './icons/GitHub';
 
 const ICON_WIDTH = 20;
 const ICON_HEIGHT = 20;
 
-/**
- * Array containing supported icon with URL regex matcher.
- */
-const SUPPORTED_ICONS = [
-  {
-    icon: (
-      <AppStore
-        width={ICON_WIDTH}
-        height={ICON_HEIGHT}
-      />
-      ),
-      regex: /.+apps\.apple\.com.+/gm,
-  },
-  {
-    icon: (
-      <GooglePlay
-        width={ICON_WIDTH}
-        height={ICON_HEIGHT}
-      />
-      ),
-      regex: /.+play\.google\.com.+/gm,
-  },
-  {
-    icon: (
-      <Figma
-        width={ICON_WIDTH}
-        height={ICON_HEIGHT}
-      />
-      ),
-      regex: /.+figma\.com.+/gm,
-  },
-];
 
 export default function ExternalLink({ name, url }: PageExternalLink) {
+  /**
+   * Array containing supported icon with URL regex matcher.
+   */
+  const SUPPORTED_ICONS = [
+    {
+      icon: (
+        <AppStore
+          width={ICON_WIDTH}
+          height={ICON_HEIGHT}
+        />
+        ),
+        regex: /.+apps\.apple\.com.+/gm,
+    },
+    {
+      icon: (
+        <GooglePlay
+          width={ICON_WIDTH}
+          height={ICON_HEIGHT}
+        />
+        ),
+        regex: /.+play\.google\.com.+/gm,
+    },
+    {
+      icon: (
+        <GitHub
+          width={ICON_WIDTH}
+          height={ICON_HEIGHT}
+        />
+        ),
+        regex: /.+github\.com.+/gm,
+    },
+    {
+      icon: (
+        <Figma
+          width={ICON_WIDTH}
+          height={ICON_HEIGHT}
+        />
+        ),
+        regex: /.+figma\.com.+/gm,
+    },
+  ];
+  
   const iconIndex = SUPPORTED_ICONS.findIndex((obj) => obj.regex.test(url));
 
   return (
