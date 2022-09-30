c8b3829ab745bbce32022cddf63db9d0c945c63f
Brendan <smallpigeon1@gmail.com>
2022-09-29 21:31:12 -0700

    update svg in component

--- components/icons-v2/ArrowUpAndRight.tsx
+++ components/icons-v2/ArrowUpAndRight.tsx
@@ -2,26 +2,8 @@ import * as React from 'react';
 
 function ArrowUpAndRight(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      width={32}
-      height={32}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <g clipPath="url(#prefix__clip0_635_6551)">
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M9 1h22v22h-2V4.414L2.707 30.707l-1.414-1.414L27.586 3H9V1z"
-          fill="#121212"
-        />
-      </g>
-      <defs>
-        <clipPath id="prefix__clip0_635_6551">
-          <path fill="#fff" transform="translate(1 1)" d="M0 0h30v30H0z" />
-        </clipPath>
-      </defs>
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path fillRule="evenodd" clipRule="evenodd" d="M9 1H31V23H29V4.41421L2.70711 30.7071L1.29289 29.2929L27.5858 3H9V1Z" fill="#121212"/>
     </svg>
   );
 }
