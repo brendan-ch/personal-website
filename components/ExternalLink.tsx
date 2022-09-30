a9f46fd84e95a81fef542a185153143486c47033
Brendan <smallpigeon1@gmail.com>
2022-09-29 20:44:10 -0700

    add external icon support

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,5 +1,34 @@
 import ArrowUpAndRight from './icons-v2/ArrowUpAndRight';
 import styles from '../styles/ExternalLink.module.css';
+import AppStore from './logos/AppStore';
+
+const ICON_WIDTH = 20;
+const ICON_HEIGHT = 20;
+
+/**
+ * Array containing supported icon with URL regex matcher.
+ */
+const SUPPORTED_ICONS = [
+  {
+    icon: (
+      <AppStore
+        width={ICON_WIDTH}
+        height={ICON_HEIGHT}
+      />
+      ),
+      regex: /.+apps\.apple\.com.+/gm,
+  },
+  // Leave last
+  {
+    icon: (
+      <ArrowUpAndRight
+        width={ICON_WIDTH}
+        height={ICON_HEIGHT}
+      />
+    ),
+    regex: /.+/gm,
+  },
+];
 
 interface Props {
   name: string,
@@ -7,13 +36,12 @@ interface Props {
 }
 
 export default function ExternalLink({ name, url }: Props) {
+  const iconIndex = SUPPORTED_ICONS.findIndex((obj) => obj.regex.test(url));
+
   return (
     <div className={styles.container}>
       {/* Icon */}
-      <ArrowUpAndRight
-        width={20}
-        height={20}
-      />
+      {SUPPORTED_ICONS[iconIndex].icon}
       {/* Text */}
       <a href={url}>
         <p>
