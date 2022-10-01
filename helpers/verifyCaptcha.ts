e992b7a48de44e9fab7d0d2e83c80cfd49356851
Brendan <smallpigeon1@gmail.com>
2022-10-01 00:32:12 -0700

    add score threshold parameter

--- helpers/verifyCaptcha.ts
+++ helpers/verifyCaptcha.ts
@@ -3,8 +3,10 @@ import axios from 'axios';
 /**
  * Verifies the captcha code provided by the client.
  * @param response
+ * @param secret
+ * @param scoreThreshold
  */
-export default async function verifyCaptcha(response: string, secret: string) {
+export default async function verifyCaptcha(response: string, secret: string, scoreThreshold?: number) {
   const url = `https://www.google.com/recaptcha/api/siteverify?response=${response}&secret=${secret}`;
 
   const result = await axios.post(url, {}, {
@@ -13,5 +15,5 @@ export default async function verifyCaptcha(response: string, secret: string) {
     },
   });
 
-  return result.data.success && result.data.score > 0.5;
+  return result.data.success && result.data.score > (scoreThreshold ? scoreThreshold : 0.5);
 }
