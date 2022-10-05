7a1216caeb860c5dee12e63f0fe86cb565996771
Brendan <smallpigeon1@gmail.com>
2022-10-05 16:28:32 -0700

    add bottom margin to tags date wrapper

--- components/DatabaseItemContent.tsx
+++ components/DatabaseItemContent.tsx
@@ -72,6 +72,7 @@ export default function DatabaseItemContent({
           {/* Text information wrapper */}
           <div className={styles.tagsDateWrapper} style={{
             flexDirection: 'column',
+            marginBottom: 24,
           }}>
             {/* Tags */}
             {tags ? (
