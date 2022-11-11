6281ead289ec931c84031dbe7fc4b5ae62a377f7
Brendan Chen <smallpigeon1@gmail.com>
2022-11-11 13:48:17 -0800

    update 404 and 500 pages with new styling

--- pages/404.page.tsx
+++ pages/404.page.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
-import ImageWithFadeIn from '../components/ImageWithFadeIn';
 import MobileNavBar from '../components/MobileNavBar';
 import MobileNavMenu from '../components/MobileNavMenu';
 import utils from '../styles/utils.module.css';
@@ -23,61 +22,63 @@ export default function Custom404() {
       <Head>
         <title>Page Not Found | Brendan Chen</title>
       </Head>
-      <MobileNavBar
-        onMobileButtonClick={() => setMenuVisible(true)}
-      />
-      <MobileNavMenu
-        visible={menuVisible}
-        onClose={() => setMenuVisible(false)}
-      />
-      <main>
-        <div className={utils.spacer} />
-        <div className={utils.itemWrapper}>
-          <PageHeader
-            belowText="404 Not Found"
+      <div className={utils.minHeightWrapper}>
+        <MobileNavBar
+          onMobileButtonClick={() => setMenuVisible(true)}
+        />
+        <MobileNavMenu
+          visible={menuVisible}
+          onClose={() => setMenuVisible(false)}
+        />
+        <main>
+          <div className={utils.itemWrapper}>
+            <PageHeader
+              belowText="Page Not Found"
             // includeBackButton
             // backButtonHref="/"
-          />
-        </div>
-        <div className={`${utils.itemWrapper} ${utils.stretchToEnd}`}>
-          <p>We couldn’t find the page you were looking for.</p>
-          <Link href="/">
-            <a>
-              <p>
-                <u>
-                  Back to Home
-                </u>
-              </p>
-            </a>
-          </Link>
-          <Image
-            src={Image404}
-            alt="Capybara in bath"
-          />
-          <p>
-            (Image by{' '}
-            <Link href="https://unsplash.com/@veverkolog">
-              <a target="_blank">
-                <u>
-                  Dušan veverkolog
-                </u>
+            />
+          </div>
+          <div className={`${utils.itemWrapper}`}>
+            <p>We couldn’t find the page you were looking for.</p>
+            <Link href="/">
+              <a>
+                <p>
+                  <u>
+                    Back to Home
+                  </u>
+                </p>
               </a>
             </Link>
-            {' on '}
-            <Link href="https://unsplash.com">
-              <a target="_blank">
-                <u>
-                  Unsplash
-                </u>
-              </a>
-            </Link>
-            )
-          </p>
-        </div>
-        <div className={utils.footerWrapper}>
-          <Footer />
-        </div>
-      </main>
+            <Image
+              src={Image404}
+              alt="Capybara in bath"
+            />
+            <p>
+              (Image by{' '}
+              <Link href="https://unsplash.com/@veverkolog">
+                <a target="_blank">
+                  <u>
+                    Dušan veverkolog
+                  </u>
+                </a>
+              </Link>
+              {' on '}
+              <Link href="https://unsplash.com">
+                <a target="_blank">
+                  <u>
+                    Unsplash
+                  </u>
+                </a>
+              </Link>
+              )
+            </p>
+          </div>
+        </main>
+      </div>
+      <div className={utils.spacer} />
+      <div className={utils.footerWrapper}>
+        <Footer />
+      </div>
     </div>
   );
 }
