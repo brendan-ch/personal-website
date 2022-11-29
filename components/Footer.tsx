3a4b9f5dd79bcfbffa6917aa59c87728208c67c8
Brendan Chen <smallpigeon1@gmail.com>
2022-11-28 17:28:05 -0800

    add "back to top" button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { CSSProperties } from 'react';
 import styles from '../styles/Footer.module.css';
+import ChevronUp from './icons-v2/ChevronUp';
 
 interface Props {
   style?: CSSProperties,
@@ -10,6 +11,19 @@ interface Props {
 export default function Footer({ style, mini }: Props) {
   return (
     <footer className={styles.container} style={style}>
+      <div className={styles.horizontalWrapper}>
+        <Link href="#top">
+          <a>
+            <p>
+              Back to top
+            </p>
+          </a>
+        </Link>
+        <ChevronUp
+          width={24}
+          height={24}
+        />
+      </div>
       <div className={styles.horizontalWrapper}>
         <Link href="/doc/privacy">
           <a>
