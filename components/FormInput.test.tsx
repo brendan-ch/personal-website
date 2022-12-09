03cf787ba9e1c899348ea2a9c03a34bd47e292f1
Brendan Chen <smallpigeon1@gmail.com>
2022-12-09 13:47:18 -0800

    test the required prop

--- components/FormInput.test.tsx
+++ components/FormInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import FormInput from './FormInput';
 import '@testing-library/jest-dom';
 
@@ -12,5 +12,41 @@ describe('FormInput', () => {
 
     const input = screen.getByRole('textbox');
     expect(input).toBeInTheDocument();
+  });
+
+  it('Test the required prop', () => {
+    render(<div>
+      <FormInput
+        placeholder="Input #1"
+        name="Input #1"
+        label="Input #1"
+        required
+      />
+      <FormInput
+        placeholder="Input #2"
+        name="Input #2"
+        label="Input #2"
+        required
+        multiline
+      />
+      <FormInput
+        placeholder="Input #3"
+        name="Input #3"
+        label="Input #3"
+      />
+      <FormInput
+        placeholder="Input #4"
+        name="Input #4"
+        label="Input #4"
+        multiline
+      />
+    </div>);
+
+    const inputs = screen.getAllByRole('textbox');
+    // Expect the first two inputs to be required
+    expect(inputs[0].getAttribute('required') === null).toBe(false);
+    expect(inputs[1].getAttribute('required') === null).toBe(false);
+    expect(inputs[2].getAttribute('required') === null).toBe(true);
+    expect(inputs[3].getAttribute('required') === null).toBe(true);
   });
 });
