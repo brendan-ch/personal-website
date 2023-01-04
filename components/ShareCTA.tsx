de49e161c84495fcb2b3e236ba5fdc8f898438a7
Brendan Chen <smallpigeon1@gmail.com>
2023-01-04 14:10:48 -0800

    add button for copying

--- components/ShareCTA.tsx
+++ components/ShareCTA.tsx
@@ -7,16 +7,25 @@ interface Props {
    * Additional share links to include, in addition to the "Copy to clipboard" button.
    */
   links: PageExternalLink[],
+  /**
+   * Link for the current page to copy.
+   */
+  copyLink: string,
 }
 
 /**
  * Add this component at the end of a project to encourage people to share it.
  * @param props
  */
-function ShareCTA({ links }: Props) {
+function ShareCTA({ links, copyLink }: Props) {
   return (
     <div className={styles.container}>
       <p>Like this project? Consider sharing it:</p>
+      <ExternalLink
+        name="Copy link"
+        url={copyLink}
+        action="copy"
+      />
       {links.map((value) => (
         <ExternalLink
           key={value.url}
