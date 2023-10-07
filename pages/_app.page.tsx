b1c28afa6a4d7bf11b254f895dc96f6614de0c91
Brendan Chen <smallpigeon1@gmail.com>
2023-10-07 09:58:37 -0700

    implement conditional layout rendering

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -5,6 +5,15 @@ import type { AppProps } from 'next/app'
 import CustomLayout from '../components/layout/CustomLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Disable layout for unified home page
+  if (process.env.NEXT_PUBLIC_UNIFIED_HOME === '1') {
+    return <>
+      {/* @ts-ignore */}
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  }
+
   return (
     <>
       <CustomLayout>
