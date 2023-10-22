e8c79907e396a207a20a76556ce44c83f1b3e503
Brendan Chen <smallpigeon1@gmail.com>
2023-10-22 15:18:37 -0700

    add ts-ignore flag

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* @ts-ignore */}
       <Component {...pageProps} />
       <Analytics />
     </>
