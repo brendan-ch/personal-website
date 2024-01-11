bb9182356bec0a993be1a766f4160e18f71e882f
Brendan Chen <smallpigeon1@gmail.com>
2024-01-11 19:11:52 +0000

    add redirects for Clockwise docs

--- next.config.js
+++ next.config.js
@@ -17,6 +17,32 @@ const nextConfig = {
         destination: '/',
         permanent: true,
       },
+      // Docs
+      {
+        source: '/doc/clockwise-privacy',
+        destination: 'https://gist.github.com/brendan-ch/74f7fd1b93a5c093d6fa9b714706d5f6',
+        permanent: true,
+      },
+      {
+        source: '/doc/clockwise-licenses',
+        destination: 'https://gist.github.com/brendan-ch/033a37ad207e1d10dc2dc1dc9f0b75f6',
+        permanent: true,
+      },
+      {
+        source: '/doc/clockwise-migrate',
+        destination: 'https://gist.github.com/brendan-ch/1c1084b817ad589c9936c3d37752a84f',
+        permanent: true,
+      },
+      {
+        source: '/doc/clockwise-whats-new',
+        destination: 'https://gist.github.com/brendan-ch/90da41b690d3579dd5e755aa7d1ac552',
+        permanent: true,
+      },
+      {
+        source: '/doc/clockwise-app',
+        destination: 'https://gist.github.com/brendan-ch/3bad40f7c4dcbd7be990411ec7f9ec50',
+        permanent: true,
+      },
       // Projects
       {
         source: '/work/headspace-logo-redesign',
