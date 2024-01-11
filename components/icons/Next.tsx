28d4155d0e73e8747f115a30810784cba9a197cd
Brendan Chen <smallpigeon1@gmail.com>
2024-01-11 19:23:17 +0000

    remove unused icons

--- components/icons/Next.tsx
+++
@@ -1,9 +0,0 @@
-import { IconProps } from "./icon-types";
-
-export default function Next({ width, height }: IconProps) {
-  return (
-    <svg width={`${width}`} height={`${height}`} viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fillRule="evenodd" clipRule="evenodd" d="M24.2786 19.9715L14.6607 10.3536L15.3678 9.64645L25.6928 19.9715L15.3678 30.2965L14.6607 29.5894L24.2786 19.9715Z" fill="#242424"/>
-    </svg>
-  )
-}
