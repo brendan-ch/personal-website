3ff0264dfa6127b55e7ae2043ef52af524936f0f
Brendan Chen <smallpigeon1@gmail.com>
2024-01-11 19:42:12 +0000

    remove work and docs pages from sitemap

--- pages/sitemap.xml.page.tsx
+++ pages/sitemap.xml.page.tsx
@@ -5,21 +5,9 @@ import getPages from '../helpers/getPages';
 export async function getServerSideProps({ req, res }: { req: NextApiRequest, res: NextApiResponse }) {
   const baseUrl = `https://${req.headers.host}`;
 
-  // Get all possible pages
-  // Get work pages
-  const workPages = await getPages({
-    prefix: 'work',
-  });
-
-  // Get doc pages
-  const docPages = await getPages({
-    prefix: 'doc',
-  });
-
   // List high-level pages here
   const highPriority = [
     '',
-    '/work',
   ];
 
   // Generate the sitemap here
@@ -29,18 +17,6 @@ export async function getServerSideProps({ req, res }: { req: NextApiRequest, re
       return `<url>
         <loc>${baseUrl}${value}</loc>
         <priority>1.0</priority>
-      </url>`;
-    }).join('')}
-    ${workPages.pageData.map((value) => {
-      return `<url>
-        <loc>${baseUrl}/work/${value.id}</loc>
-        <priority>0.5</priority>
-      </url>`;
-    }).join('')}
-    ${docPages.pageData.map((value) => {
-      return `<url>
-        <loc>${baseUrl}/doc/${value.id}</loc>
-        <priority>0.0</priority>
       </url>`;
     }).join('')}
   </urlset>`;
