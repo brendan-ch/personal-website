7c95a8f36f79e087ffb544675d946c3a2ff19e4d
Brendan Chen <smallpigeon1@gmail.com>
2024-01-15 23:50:22 +0000

    update 404 content and styling

--- pages/404.page.tsx
+++ pages/404.page.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import utils from '../styles/utils.module.css';
 
-import Image404 from '../public/404.jpg';
-import PageHeader from '../components/content/PageHeader';
-import Image from 'next/image';
+import Hero from '../components/content/Hero';
+import Link from 'next/link';
+import styles from './404.module.css';
 
 /**
  * Custom 404 page.
@@ -16,51 +14,41 @@ export default function Custom404() {
       <Head>
         <title>Page Not Found | Brendan Chen</title>
       </Head>
-      <div className={utils.itemWrapper}>
-        <PageHeader
-          belowText="Page Not Found"
-        // includeBackButton
-        // backButtonHref="/"
-        />
-      </div>
-      <div className={`${utils.itemWrapper}`}>
-        <p>We couldn’t find the page you were looking for.</p>
-        <Link href="/">
-
+      <Hero
+        imageAlt=""
+        imagePath="/404.jpg"
+      >
+        <h1>Page not found</h1>
+        <div className={styles.textContainer}>
+          <p>We couldn{"'"}t find the page you were looking for; it may have been moved or deleted.</p>
+          <Link href="/">
+            <p>
+              <u>
+                Back to Home
+              </u>
+            </p>
+          </Link>
           <p>
-            <u>
-              Back to Home
-            </u>
+            (Image by{' '}
+            <Link href="https://unsplash.com/@veverkolog" target="_blank">
+
+              <u>
+                Dušan veverkolog
+              </u>
+
+            </Link>
+            {' on '}
+            <Link href="https://unsplash.com" target="_blank">
+
+              <u>
+                Unsplash
+              </u>
+
+            </Link>
+            )
           </p>
-
-        </Link>
-        <div className={utils.pageImageContainer}>
-          <Image
-            src={Image404}
-            alt="Capybara in bath"
-            fill
-          />
         </div>
-        <p>
-          (Image by{' '}
-          <Link href="https://unsplash.com/@veverkolog" target="_blank">
-
-            <u>
-              Dušan veverkolog
-            </u>
-
-          </Link>
-          {' on '}
-          <Link href="https://unsplash.com" target="_blank">
-
-            <u>
-              Unsplash
-            </u>
-
-          </Link>
-          )
-        </p>
-      </div>
+      </Hero>
     </main>
   );
 }
