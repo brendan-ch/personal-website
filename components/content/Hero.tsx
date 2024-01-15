2f7028e2ffed3b13f1504b66611d225a23cb8289
Brendan Chen <smallpigeon1@gmail.com>
2024-01-15 23:20:57 +0000

    define props for Hero component

---
+++ components/content/Hero.tsx
@@ -0,0 +1,20 @@
+interface Props extends React.PropsWithChildren {
+  /**
+   * Path to an image to display.
+   * This can be a local path within the `public` folder,
+   * or it can be a remote URL.
+   * 
+   * Domains of remote URLs must be configured in next.config.js.
+   * See https://nextjs.org/docs/pages/building-your-application/optimizing/images#domains for more info.
+   */
+  imagePath: string,
+
+  /**
+   * Alt text for the hero image.
+   */
+  alt: string,
+}
+
+export default function Hero({ imagePath, alt, children }: Props) {
+  return <></>
+}
