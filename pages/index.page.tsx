9c145cdcc9ab5a7885364ef26f8c761a7fe74501
Brendan Chen <smallpigeon1@gmail.com>
2024-01-23 07:23:35 +0000

    change award wording

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -68,7 +68,7 @@ function UnifiedHome() {
               url: 'https://cruzhacks.com',
             }]}
           title="Power to the People"
-          description="A mobile app designed to make traffic stops safer, developed during CruzHacks 2024. Record interactions with police, assign and text emergency contacts, and be informed of your rights. I worked with a team of 4 to build a minimum viable product using SwiftUI, in a period of 36 hours. The app was selected for the President's Pick award."
+          description="A mobile app designed to make traffic stops safer, developed during CruzHacks 2024. Record interactions with police, assign and text emergency contacts, and be informed of your rights. I worked with a team of 4 to build a minimum viable product using SwiftUI, in a period of 36 hours. The app was selected for the Organizer's Pick award."
           imagePath="/static/ufh/power-to-the-people-01.png"
           imageAlt="Preview image for Power to the People"
         />
