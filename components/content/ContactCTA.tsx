8fcbad6ae7c425a69b1b010393a9e5ba0cb4a874
Brendan Chen <me@bchen.dev>
2024-02-15 16:26:35 -0800

    add ContactCTA component

---
+++ components/content/ContactCTA.tsx
@@ -0,0 +1,15 @@
+import ActionButton from '../ActionButton';
+
+import styles from 'ContactCTA.module.css';
+
+export default function Contact() {
+  return (
+    <div className={styles.contact}>
+      <h1>Let{"'"}s work together.</h1>
+      <div className={styles.contactButtons}>
+        <ActionButton text="View resume" href="/resume.pdf" useRegularLink />
+        <ActionButton text="me@bchen.dev" href="mailto:me@bchen.dev" highlighted />
+      </div>
+    </div>
+  )
+}
