818d789215d3f288e5579359ef649cf6e5fa1100
Brendan Chen <me@bchen.dev>
2024-03-18 15:08:51 -0700

    add GalleryScrollFocus context

--- components/content/GalleryScrollFocus.tsx
+++ components/content/GalleryScrollFocus.tsx
@@ -18,6 +18,15 @@ interface Props extends React.PropsWithChildren {
   images: GalleryImage[],
 }
 
+/**
+ * Component which gains the user's attention by focusing on something specific.
+ * Pass in a gallery of images which scroll while the children elements stick
+ * to the viewport.
+ * @param param0
+ * @returns
+ * 
+ * @see {Figma component}(https://www.figma.com/file/Dal59aHrblUpA2afXrThW7/bchen.dev?type=design&node-id=1701-1475&mode=design&t=7Zzw0Yri5WF9ynbK-11)
+ */
 export default function GalleryScrollFocus({ images }: Props) {
   return (
     <></>
