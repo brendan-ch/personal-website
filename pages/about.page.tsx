5b0692bb04b70c0344491411684cd63ea3880745
Brendan Chen <me@bchen.dev>
2024-03-18 17:33:44 -0700

    add text styling

--- pages/about.page.tsx
+++ pages/about.page.tsx
@@ -27,14 +27,14 @@ function About() {
       </Focus>
       <GalleryScrollFocus images={[]}>
         <h2>In August 2022, I started studying <b>computer science</b> at Chapman University's <b>Fowler School of Engineering</b>.</h2>
-        <p>I chose this path because I view engineering as an art form, a method of communication with an audience that spans billions. Computer science is the means to understanding the toolset required for success.</p>
+        <p>I chose this path because I view <b>engineering as an art form</b>, a method of communication with an audience that spans billions. Computer science is the means to understanding the toolset required for success.</p>
       </GalleryScrollFocus>
       <GalleryScrollFocus images={[]}>
         <p>Through a strong focus on academics, I was selected for the Provost’s List award in spring and fall 2023. I also participate in several engineering-related clubs and served as the graphic designer for Chapman’s iOS Developers Club.</p>
-        <p>In January 2024, I attended CruzHacks 2024 representing Chapman’s Computer Science Club, winning the President’s Pick award with our project.</p>
+        <p>In January 2024, I attended <b>CruzHacks 2024</b> representing Chapman’s <b>Computer Science Club</b>, winning the President’s Pick award with our project.</p>
       </GalleryScrollFocus>
       <GalleryScrollFocus images={[]}>
-        <p>In the future, I aspire to be a software engineer who challenges the status quo. I consider myself a calculated risk-taker, a critical thinker, who stops at nothing and dares to dream about future ambitions.</p>
+        <p>In the future, I aspire to be a <b>software engineer</b> who challenges the status quo. I consider myself a calculated risk-taker, a critical thinker, who stops at nothing and <b>dares to dream</b> about future ambitions.</p>
       </GalleryScrollFocus>
       <ContactCTA />
       <Footer />
